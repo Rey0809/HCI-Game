5bac6636a8da3f0886e9347ef8284f1f68faef28
AKoehle89 <Ann-Katrin@Koehler-Zeithain.de>
2026-06-18 09:43:23 +0200

    eval-fixing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781767901|954639950';
+const CACHE_VERSION = '1781768592|1645683029';
 /** @type {string} */
 const CACHE_PREFIX = 'Bachelor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
