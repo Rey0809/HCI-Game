2fb859b3ef50d995760396519f500beec0e488a1
AKoehle89 <Ann-Katrin@Koehler-Zeithain.de>
2026-06-18 09:28:04 +0200

    adjusted tracking

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781701278|789027389';
+const CACHE_VERSION = '1781767665|719148849';
 /** @type {string} */
 const CACHE_PREFIX = 'Bachelor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
